67eb690e071e088c811e0700b412bc950883529e
Rouzbeh Meshkinnejad <rouzyd@gmail.com>
2021-03-26 20:58:32 +0430

    Part 3: tests

---
+++ src/main/java/com/selab/tdd/Square.java
@@ -0,0 +1,17 @@
+package com.selab.tdd;
+
+public class Square {
+    public Square(double v) {
+    }
+
+    public double computeArea() {
+        return 0;
+    }
+
+    public void setSide(double i) {
+    }
+
+    public double getSide() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/selab/tdd/SquareTest.java
@@ -0,0 +1,73 @@
+package com.selab.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class SquareTest {
+    @Test
+    public void negativeSideTest()
+    {
+        try
+        {
+            Square square = new Square(-5.32);
+            fail("Expected NegativeSideException did not occur.");
+            throw new NegativeSideException();
+        }
+        catch (NegativeSideException e)
+        {}
+
+        try {
+            Square square = new Square(-2321.214);
+            fail("Expected NegativeSideException did not occur.");
+            throw new NegativeSideException();
+        }
+        catch (NegativeSideException e)
+        {}
+
+        try {
+            Square square = new Square(-0.001);
+            fail("Expected NegativeSideException did not occur.");
+            throw new NegativeSideException();
+        }
+        catch (NegativeSideException e)
+        {}
+    }
+
+    @Test
+    public void computeAreaTest()
+    {
+        double delta = 1e-4;
+        Square square1 = new Square(5.32);
+        assertEquals(5.32*5.32, square1.computeArea(), delta);
+
+        Square square2 = new Square(1234.1234);
+        assertEquals(1234.1234*1234.1234, square2.computeArea(), delta);
+
+        Square square3 = new Square(0.987);
+        assertEquals(0.987*0.987, square3.computeArea(), delta);
+    }
+
+    @Test
+    public void setterAndGetterTest()
+    {
+        double delta = 1e-4;
+
+        Square square = new Square(4);
+        square.setSide(5);
+        assertEquals(square.getSide(), 5, delta);
+
+        square.setSide(123.456);
+        assertEquals(square.getSide(), 123.456, delta);
+
+        try
+        {
+            square.setSide(-11);
+            fail("Expected NegativeSideException did not occur.");
+            throw new NegativeSideException();
+        }
+        catch (NegativeSideException e)
+        {}
+    }
+}
